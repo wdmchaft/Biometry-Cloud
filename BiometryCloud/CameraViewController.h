321b5981c8608fb989a2b85d7434e159ae90b9c3
Andres Munita <amunita@gmail.com>
2011-10-11 11:10:24 -0300

    nuevos cambios

---
+++ BiometryCloud/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  BiometryCloud
+//
+//  Created by Andrés Munita Irarrázaval on 06-10-11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
---
+++ BiometryCloud/iPad/CameraViewController_iPad.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController_iPad.h
+//  BiometryCloud
+//
+//  Created by Andrés Munita Irarrázaval on 06-10-11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController_iPad : UIViewController
+
+@end
