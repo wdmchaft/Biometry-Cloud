b7c6248559b66f0ccf668f862f257a04b51fe43b
Pablo Mandiola <pmandiolab@gmail.com>
2011-10-11 14:46:26 -0300

    opencv build

---
+++ BiometryCloud/BiometryDetector.h
@@ -0,0 +1,16 @@
+//
+//  ByometryDetector.h
+//  BiometryCloud
+//
+//  Created by Pablo Mandiola on 10/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ByometryDetector : NSObject {
+    
+}
+
+@end
---
+++ BiometryCloud/BiometryDetector.m
@@ -0,0 +1,14 @@
+//
+//  ByometryDetector.m
+//  BiometryCloud
+//
+//  Created by Pablo Mandiola on 10/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ByometryDetector.h"
+
+
+@implementation ByometryDetector
+
+@end
